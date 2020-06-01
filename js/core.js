bb90dc897350fec50d83b34c7d151ac2043d670e
ostov-larion <39136591+ostov-larion@users.noreply.github.com>
2020-06-01 23:41:30 +0500

    Add files via upload

---
+++ js/core.js
@@ -0,0 +1,13 @@
+
+let SDK = datSdk()
+
+Hyperdrive = SDK.Hyperdrive()
+console.log(Hyperdrive)
+/*
+let archive = Hypercore.load(`dat://dat.foundation`)
+
+// Read a file and parse it as JSON
+let  datJSON = JSON.parse(await archive.readFile('/dat.json'))
+
+console.log(datJSON)*/
+
--- js/main.js
+++ js/main.js
@@ -1,33 +1,22 @@
-Board = {
-    view: () =>
-        m('#board',[
-            m('.card', m('.chips#search')),
-            m('.center-align',{style:{position:'absolute',top:screen.height/2,left:screen.width/2.3}}, m(Preloader,{active:isLoading})),
-            m('.fixed-action-btn',m('a.btn-floating.btn-large.teal.modal-trigger',{href:'#addContent'},m('i.material-icons.large.white-text','add'))),
-            m(Masonry),
-            m(Modal,{
-                id:'addContent',
-                content: [
-                    m(FileInput),
-                    m('.chips#tags')
-                ],
-                footer:[
-                    m('.btn-flat.modal-close','Upload')
-                ]
-            })
-        ]),
-    oncreate() {
-        chipsInit()
-    }
+let {Tabs, Button, FloatingActionButton, RoundIconButton, FileInput, ModalPanel, Chips, MaterialBox} = materialized
+
+class Router{
+	#current = ""
+	constructor(routes,current){
+		this.routes = routes
+		this.#current = routes[current]
+	}
+	set current(v){
+		this.#current = this.routes[v]
+		m.redraw()
+	}
+	get current(){
+		return this.#current
+	}
 }
+
 isLoading = false
-function Search(string){
-    console.log(string)
-}
-function debug(value){
-    console.log(value)
-    return value
-}
+
 Preloader = {
     view: (vnode) =>
         m('.preloader-wrapper',{class:vnode.attrs.active?'active':''},[
@@ -44,18 +33,75 @@ Preloader = {
             ])
         ])
 }
-FileInput = {
+
+MasonryState = {
+    contents: [],
+    visible: true,
+    add: (file,tags) => {
+        let fr = new FileReader()
+        fr.readAsDataURL(file)
+        fr.onload = () => {
+            MasonryState.contents.push({image:fr.result,tags})
+            m.redraw()
+        }
+    }
+}
+
+Masonry = {
     view: () =>
-        m('.file-field.input-field',[
-            m('.btn',[
-                'File',
-                m('input[type=file]')
-            ]),
-            m('.file-path-wrapper',[
-                m('input.file-path.validate[type=text]')
-            ])
+        m('.masonry',{
+            style:{display: MasonryState.visible?'block':'none'},
+        },
+            MasonryState.contents.map(({image,tags}) => m(MasonryItem,m('img',{
+				src: image,
+				onclick: () => {
+					ContentModalState.src = image
+					ContentModalState.tags = tags
+					App.rerender()
+				}
+			})))
+        )
+}
+
+MasonryItem = {
+    view: (vnode) =>
+    m('a.masonry-item.box.dynamic.modal-trigger[href=#contentModal]',vnode.attrs,[
+        m('.masonry-content',vnode.children)
+    ])
+}
+
+Board = {
+    view: () =>
+        m('#board',[
+            m('.card',
+				m(Chips,{
+					id:'search',
+					placeholder: ' Search with tags',
+					secondaryPlaceholder: '+Tag'
+				})
+			),
+            m('.center-align',{style:{position:'absolute',top:screen.height/2,left:screen.width/2.3}}, m(Preloader,{active:isLoading})),
+            m('.fixed-action-btn',
+				m(RoundIconButton,{
+					className: 'teal',
+					modalId: 'addContent',
+					iconName: 'add'
+				})
+			),
+			addContentModal(),
+			m(Modal,{
+				id: 'contentModal',
+				content:[
+					m(MaterialBox,{src: ContentModalState.src,width:document.width>600?"30%":"90%"}),
+					m(Chips,{
+						data: ContentModalState.tags
+					})
+				]
+			}),
+            m(Masonry),
         ])
 }
+
 Modal = {
     view: (vnode) =>
         m('.modal',{id:vnode.attrs.id},[
@@ -63,75 +109,96 @@ Modal = {
             vnode.attrs.footer?m('.modal-footer',vnode.attrs.footer):''
         ])
 }
+
+UploadState = {
+	file: {},
+	tags: []
+}
+
+addContentModal = () => m(Modal,{
+    id:'addContent',
+    content: [
+        m(FileInput,{
+			id:'file',
+			placeholder:'Upload file',
+			accept: ['image/*'],
+			onchange: ([file]) => UploadState.file = file
+		}),
+        m(Chips,{
+			id:'tags',
+			placeholder: 'Add a tag',
+			secondaryPlaceholder: '+Tag',
+			data: UploadState.tags,
+			onchange: data => {
+				UploadState.tags = data
+				console.log(data)
+			}
+		})
+    ],
+    footer:[
+        m('.btn-flat.modal-close',{
+            onclick: () => {
+                MasonryState.add(UploadState.file,UploadState.tags)
+				//UploadState.tags = []
+				App.rerender()
+            }
+        },'Upload')
+    ]
+})
+
+ContentModalState = {
+    src: "",
+    tags: []
+}
+
+
 About = {
     view: ()=>
         m('.card','About')
 }
-Routes = {
-    '#board': Board,
-    '#about': About
-}
-Route = Routes['#board']
-changeRoute = route => {
-    Route = Routes[route];
-    m.redraw()
-}
-chipsInit = () =>
-    M.Chips.init(document.querySelector('#search'),{
-    placeholder:'Enter Tag',
-    secondaryPlaceholder:'+Tag',
-    onChipAdd: () => {
-        document.querySelectorAll('.chip').forEach(el => el.innerText.includes('not:') && (el.style='background-color:#e57373'))
-        isLoading = true
-        MasonryState.visible = false
-        m.redraw()
-        setTimeout(()=>{isLoading = false; MasonryState.visible = true; m.redraw()},1000)
-    }
+
+
+router = new Router({
+	'#board': Board,
+	'#about': About
 })
-MasonryState = {
-    contents: ['tests/1.png','tests/2.png','tests/3.png','tests/4.png','tests/5.png'],
-    visible: true,
-    add: (image) => {
-        //let image = await new FileReader().readAsDataURL(file)
-        MasonryState.contents.push(image)
-        m.redraw()
-    }
-}
-Masonry = {
-    view: () =>
-        m('.masonry',{
-            style:{display: MasonryState.visible?'block':'none'}
-        },
-            MasonryState.contents.map(image => m(MasonryItem,m('img',{src:image})))
-        )
-}
-MasonryItem = {
-    view: (vnode) =>
-    m('.masonry-item.box.dynamic',[
-        m('.masonry-content',vnode.children)
-    ])
-}
-App = {
-    rerender: () => {m.redraw();M.AutoInit()},
-    view: () =>
-        m('#app', [
-            m('.navbar-fixed',
+
+router.current = '#board'
+
+Nav = {
+	view: () =>
+		m('.navbar-fixed',
             m('nav.nav-wrapper', {class: 'grey lighten-5',style:'user-select:none;'}, [
                 m('a.brand-logo', {href: '#!'},m('img#logo',{src: 'logo.png',height: 55,style: {marginLeft: 10,marginTop: 2}})),
                 m('a.sidenav-trigger', {'data-target': "mobile-sidebar"}, m('i.material-icons.black-text','menu')),
                 m('ul.right.hide-on-med-and-down', [
-                    m('li',m('a.black-text.sidenav-close',{href: '#board',onclick: ()=>changeRoute('#board')},'Доска')),
-                    m('li',m('a.black-text.sidenav-close',{href: '#about',onclick:()=>changeRoute('#about')},'О Kopichan'))
+                    m('li',m('a.black-text.sidenav-close',{href: '#board',onclick: ()=> router.current = '#board'},'Доска')),
+                    m('li',m('a.black-text.sidenav-close',{href: '#about',onclick: ()=> router.current = '#about'},'О Kopichan'))
                 ]),
-            ])),
-            m('ul.sidenav#mobile-sidebar', [
-                m('li',m('a.black-text.sidenav-close',{href: '#board',onclick: ()=>changeRoute('#board')},'Доска')),
-                m('li',m('a.black-text.sidenav-close',{href: '#about',onclick:()=>changeRoute('#about')},'О Kopichan'))
+            ])
+		),
+	oncreate: () => {
+		M.AutoInit()
+		
+	}
+}
+
+App = {
+	rerender: () => {
+		m.redraw()
+		M.AutoInit()
+	},
+    view: () =>
+        m('#app', [
+			m(Nav),
+			m('ul.sidenav#mobile-sidebar', [
+                m('li',m('a.black-text.sidenav-close',{href: '#board',onclick: ()=>router.current = '#board'},'Доска')),
+                m('li',m('a.black-text.sidenav-close',{href: '#about',onclick:()=>router.current = '#about'},'О Kopichan'))
             ]),
-            m('main',m(Route))
+            m('main',m(router.current))
         ])
 }
+
 m.mount(document.body,App)
-M.AutoInit()
-chipsInit()
+
 document.querySelector('.drag-target').style = 'z-index:999;width:20px'
