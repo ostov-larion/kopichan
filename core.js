8b40241a81cb81eed81f5445fc39aad07987b2f2
ostov-larion <39136591+ostov-larion@users.noreply.github.com>
2020-06-06 02:09:33 +0500

    Add files via upload

--- core.js
+++ core.js
@@ -3,43 +3,45 @@ peer = new Peer({
 	host: 'kopichan-server.herokuapp.com',
 	port: '',
 	path: '/kopi',
-    secure: true
+	config: {
+		iceServers: [{"url":"stun:global.stun.twilio.com:3478?transport=udp","urls":"stun:global.stun.twilio.com:3478?transport=udp"},{"url":"turn:global.turn.twilio.com:3478?transport=udp","username":"a710e11718e0a3cc5ea11080056221c0991eba20fd87ed62917fc79689fb5ae5","urls":"turn:global.turn.twilio.com:3478?transport=udp","credential":"+mwEzi1TwrrzPh/Q6v/qThJktpjXaxPA38ljzqmN5Ew="},{"url":"turn:global.turn.twilio.com:3478?transport=tcp","username":"a710e11718e0a3cc5ea11080056221c0991eba20fd87ed62917fc79689fb5ae5","urls":"turn:global.turn.twilio.com:3478?transport=tcp","credential":"+mwEzi1TwrrzPh/Q6v/qThJktpjXaxPA38ljzqmN5Ew="},{"url":"turn:global.turn.twilio.com:443?transport=tcp","username":"a710e11718e0a3cc5ea11080056221c0991eba20fd87ed62917fc79689fb5ae5","urls":"turn:global.turn.twilio.com:443?transport=tcp","credential":"+mwEzi1TwrrzPh/Q6v/qThJktpjXaxPA38ljzqmN5Ew="}]
+	},
+    secure: true,
+	debug: 3
 })
-
 
 MainScheme = {
     keyPath: "hash",
     indexes: [
         {name: "hash", unique: true},
         {name: "tags"},
-        {name: "file"}
+        {name: "file"},
+		{name: "options"}
     ]
 }
+page = 0
+
+isLoading = true
 
 peer.on("open", async() => {
     octo = new OctoDB(peer)
     main = await octo.open('main',MainScheme)
     main.on('add', value => MasonryState.add(value.file,value.tags))
-	setTimeout( async() => {
-		main.on("sync", async(list) => {
-			if(list.length == 1){
-				MasonryState.contents = await main.getAllLocally()
-				//isLoading = false
-				m.redraw()
-				main.getAll()
-			}
-			else{
-				MasonryState.contents = await main.getAllLocally()
-				main.getAll()
-				m.redraw()
-			}
-			main.on("post", async() => {
-				MasonryState.contents = await main.getAllLocally()
-				//isLoading = false
-				m.redraw()
-			})
+	isLoading = false
+	m.redraw()
+	main.on("sync", async(list) => {
+		main.getPageLocally(page,10)
+		main.on('page', value => {
+			MasonryState.add(value.hash,value.file,value.tags)
 		})
-	},1000)
+		setTimeout(() => main.getPage(page,30),1000)
+		main.on("post", async(data) => {
+			!MasonryState.contents.find(e => e.hash == data.hash) && MasonryState.add(data.hash,data.file,data.tags)
+		})
+		main.on("add", async(data) => {
+			MasonryState.add(data.hash,data.file,data.tags)
+		})
+	})
 })
 
 })()
---
+++ octo-db.js
@@ -0,0 +1,310 @@
+class OctoDB {
+    constructor(peer){
+        this.peer = peer
+        this.created = false
+    }
+    async open(name,scheme){
+        return await new Promise( (resolve,reject) => {
+            let request = indexedDB.open("OctoDB/"+name,1)
+            request.onupgradeneeded = event => {
+                this.created = true
+                let db =  event.target.result
+                let store = db.createObjectStore(name,{keyPath: scheme.keyPath})
+                for(let index of scheme.indexes){
+                    store.createIndex(index.name, index.name,{unique: index.unique})
+                }
+                resolve(new OctoStoreTransaction(name,db,scheme,this.peer))
+            }
+            request.onsuccess = event =>{
+                if(this.created) return
+                let db =  event.target.result
+                resolve(new OctoStoreTransaction(name,db,scheme,this.peer))
+            }
+            request.onerror = () => {
+                reject(request.error)
+            }
+        })
+    }
+}
+
+class EventEmmiter {
+    on(eventName, handler) {
+      if (!this._eventHandlers) this._eventHandlers = {};
+      if (!this._eventHandlers[eventName]) {
+        this._eventHandlers[eventName] = [];
+      }
+      this._eventHandlers[eventName].push(handler);
+    }
+    off(eventName, handler) {
+      let handlers = this._eventHandlers && this._eventHandlers[eventName];
+      if (!handlers) return;
+      for (let i = 0; i < handlers.length; i++) {
+        if (handlers[i] === handler) {
+          handlers.splice(i--, 1);
+        }
+      }
+    }
+    trigger(eventName, ...args) {
+      if (!this._eventHandlers || !this._eventHandlers[eventName]) {
+        return; 
+      }
+      this._eventHandlers[eventName].forEach(handler => handler.apply(this, args));
+    }
+}
+
+class OctoStoreTransaction extends EventEmmiter {
+    #db = {}
+    constructor(name,db,scheme,peer){
+        super()
+        this.name = name
+        this.scheme = scheme
+        this.#db = db
+        this.peer = peer
+        this.sync()
+		this.peerList = []
+    }
+    add(value){
+        this.scheme.beforeAdd && this.scheme.beforeAdd(value)
+        let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+        let request = store.add(value)
+        this.dispatch({add: value})
+        this.trigger("add",value)
+        return new Promise((resolve, reject) => {
+            request.onsuccess = () => resolve(value)
+            request.onerror = () => reject(request.error)
+        })
+    }
+    addLocally(value){
+        this.scheme.beforeAdd && this.scheme.beforeAdd(value)
+        let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+        let request = store.add(value)
+        this.trigger("add",value)
+        return new Promise((resolve, reject) => {
+            request.onsuccess = () => resolve(value)
+            request.onerror = () => reject(request.error)
+        })
+    }
+    put(value){
+        this.scheme.beforePut && this.scheme.beforePut(value)
+        let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+        let request = store.put(value)
+        this.dispatch({put: value})
+        this.trigger("put",value)
+        return new Promise((resolve, reject) => {
+            request.onsuccess = () => resolve(value)
+            request.onerror = () => reject(request.error)
+        })
+    }
+    putLocally(value){
+        this.scheme.beforePut && this.scheme.beforePut(value)
+        let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+        let request = store.put(value)
+        this.trigger("put",value)
+        return new Promise((resolve, reject) => {
+            request.onsuccess = () => resolve(value)
+            request.onerror = () => reject(request.error)
+        })
+    }
+    delete(key){
+        this.scheme.beforeDelete && this.scheme.beforeDelete(value)
+        let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+        let request = store.delete(key)
+        this.dispatch({delete: key})
+        this.trigger("delete",key)
+        return new Promise((resolve, reject) => {
+            request.onsuccess = () => resolve(key)
+            request.onerror = () => reject(request.error)
+        })
+    }
+    deleteLocally(key){
+        this.scheme.beforeDelete && this.scheme.beforeDelete(value)
+        let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+        let request = store.delete(key)
+        this.trigger("delete",key)
+        return new Promise((resolve, reject) => {
+            request.onsuccess = () => resolve(key)
+            request.onerror = () => reject(request.error)
+        })
+    }
+    get(key){
+        return new Promise( async(resolve) => {
+            let value = await this.getLocally(key)
+            if(value){
+                resolve(value)
+            }
+            else {
+                this.dispatch({get: key})
+                let handler = this.on("post",post => {
+                    post.key == key && resolve(post.post)
+                    this.off("post",handler)
+                })
+            }
+        })
+    }
+    getLocally(key){
+        this.scheme.beforeGet && this.scheme.beforeGet(key)
+        let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+        let request = store.get(key)
+        return new Promise((resolve, reject) => {
+            request.onsuccess = event => {
+                resolve(event.target.result)
+            }
+            request.onerror = () => reject(request.error)
+        })
+    }
+    getAll(){
+        this.scheme.beforeGetAll && this.scheme.beforeGetAll()
+        let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+        let request = store.getAll()
+        return new Promise((resolve, reject) => {
+            request.onsuccess = event => {
+                this.dispatch({getAll: true})
+                resolve(event.target.result)
+            }
+            request.onerror = () => reject(request.error)
+        })
+    }
+	async getPage(page,pageSize){
+		this.scheme.beforeGetPage && this.scheme.beforeGetPage(page,pageSize)
+		this.dispatch({getPage: {page,pageSize,exept: await this.getAllKeys()}})
+	}
+	getPageLocally(page,pageSize){
+		this.scheme.beforeGetPage && this.scheme.beforeGetPage(page,pageSize)
+		let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+		let i = 0
+		store.openCursor().onsuccess = event => {
+			let cursor = event.target.result
+			if(cursor){
+				i++
+				if(i > page * pageSize && i < (page * pageSize) + pageSize){
+					this.trigger("page",cursor.value)
+					cursor.continue()
+				}
+			}
+		}
+	}
+	postPage(page,pageSize,exept){
+		let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+		let i = 0
+		store.openCursor().onsuccess = event => {
+			let cursor = event.target.result
+			if(cursor){
+				i++
+				if(i > page * pageSize && i < (page * pageSize) + pageSize){
+					if(!exept.includes(cursor.key)){
+						this.dispatch({post: cursor.value})
+					}
+					cursor.continue()
+				}
+			}
+		}
+	}
+	getAllLocally(){
+		this.scheme.beforeGetAll && this.scheme.beforeGetAll()
+        let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+        let request = store.getAll()
+        return new Promise((resolve, reject) => {
+            request.onsuccess = event => {
+                resolve(event.target.result)
+            }
+            request.onerror = () => reject(request.error)
+        })
+	}
+	openCursor(onsuccess){
+		let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+		let i = 0
+		store.openCursor().onsuccess = onsuccess
+	}
+	getAllKeys(){
+		let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+		let request = store.getAllKeys()
+		return new Promise((resolve, reject) => {
+            request.onsuccess = event => {
+                resolve(event.target.result)
+            }
+            request.onerror = () => reject(request.error)
+        })
+	}
+    count(key){
+        let store = this.#db.transaction(this.name,"readwrite").objectStore(this.name)
+        return new Promise( resolve => store.count(key).onsuccess = event => resolve(event.target.result))
+    }
+    async has(key){
+        return await this.count(key) > 0
+    }
+    async sync(){
+        let ctx = this
+        this.peer.on("connection", connection => {
+            if(this.peer.connections[connection.peer].length>=3) return
+            console.log("New peer")
+            let answer = this.peer.connect(connection.peer)
+            connection.on("data", async(data) => {
+                console.log("Data:", data)
+                if(data.getAll) {
+                    let s = await ctx.getAllLocally()
+                    this.dispatch({ postAll: s })
+                }
+				if(data.getPage) {
+                    ctx.postPage(data.getPage.page,data.getPage.pageSize,data.getPage.exept)
+                }
+                if(data.get) {
+                    let s = await ctx.getLocally(data.get)
+                    answer.send({post: s, key: data.get})
+                }
+                if(data.post){
+                    try {
+                        this.addLocally(data.post)
+                        this.trigger("post",data.post)
+                    }
+                    catch(e){}
+                }
+                if(data.postAll) {
+                    for(let entry of data.postAll){
+                        try {
+                            this.addLocally(entry)
+                            this.trigger("postAll",data.post)
+                        }
+                        catch(e){}
+                    }
+                }
+                if(data.add) {
+                    try {
+                        this.addLocally(data.add)
+                    }
+                    catch(e){}
+                }
+				if(data.put) {
+                    try {
+                        await this.putLocally(data.put)
+                        this.trigger("put",data.put)
+                    }
+                    catch(e){}
+                }
+				if(data.delete){
+					try {
+                        this.deleteLocally(data.delete)
+                    }
+                    catch(e){}
+				}
+            })
+        })
+        this.peer.listAllPeers(list => {
+            console.log("Peers:", list)
+			this.peerList = list
+            if(!list) return 
+            for(let id of list){
+                if(id != this.peer.id) {
+                    this.peer.connect(id)
+                }
+            }
+			this.trigger("sync",list)
+        })
+    }
+    dispatch(data){
+        for(let p in this.peer.connections) {
+            if(this.peer.connections[p]){
+                for(let i in this.peer.connections[p]) this.peer.connections[p][i].send(data)
+            }
+        }
+    }
+}
